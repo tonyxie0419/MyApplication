83b0f937cab7c8584f150068cb66a3a95ac05251
tonyxie <tonyxie_0419@qq.com>
2017-08-14 17:07:46 +0800

    第二次提交

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fun printString(){
-            println("1234")
+            println("12345")
         }
     }
 }
