8ebe623654cde80156ac5879a540bb660db0f489
tonyxie <tonyxie_0419@qq.com>
2017-08-14 17:01:22 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        fun printString(){
+            println("1234")
+        }
+    }
+}
